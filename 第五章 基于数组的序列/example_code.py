04510c5fd207381bb686b14e797b20113b1c6914
Rhett <jhsyzxy@gmail.com>
2020-04-06 16:51:20 +0800

    1. renamed:test_dynamic_array.py -> example_code.py 用于保存保存一些该章节中的小巧的测试函数 2. 添加代码段5-1 代码段5-4

---
+++ 第五章 基于数组的序列/example_code.py
@@ -0,0 +1,37 @@
+import sys
+from time import time
+
+
+# 代码段5-1 验证python的列表使用动态数组技术
+def verify_python_list_use_dynamic_array():
+    # list of test
+    data = []
+    # finally num of data
+    n = 10  
+    for k in range(n):
+        # 获得data的长度
+        a = len(data)
+        # 获得data所占用的字节数
+        b = sys.getsizeof(data)
+        # 打印
+        print('Length: {0:3d}; size in bytes: {1:4d}'.format(a, b))
+        # 给data增加一个元素
+        data.append(None)
+
+
+# 代码段5-4 测试python列表类增添操作的摊销花费
+def compute_average(n):
+        data = []
+        start = time()
+        for k in range(n):
+                data.append(None)
+        end = time()
+        return (end - start) / n
+
+# # 测试代码段5-1
+# verify_python_list_use_dynamic_array()
+
+# 测试代码段5-4
+for i in range(4, 10):
+        n = 10**i
+        print(compute_average(n))
